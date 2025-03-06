f380b68ca677f959a72074d344698b5f46163fb2
Sophievcy <sophievc@shaw.ca>
2025-03-06 11:18:37 -0800

    generating random numbers for sampling F07 in 2024 data

--- data_upload.R
+++ data_upload.R
@@ -6,3 +6,10 @@ Baboon_trial_data <- read.csv("gt.txt", header = FALSE)
 View(Baboon_trial_data)
 #add column names from CVAT export data site
 colnames(Baboon_trial_data) <- c("frame_id","track_id", "x", "y","w","h","not_ignored","class_id","visibility")
+
+#Sampling for F07 2024 
+set.seed(22)
+
+# Generate 88 random numbers from 1 to 120
+random_numbers <- sample(1:120, 88, replace = FALSE)
+random_numbers
