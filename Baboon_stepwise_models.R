a3e9bfdcd07e1f7e8d949d31d5f91ef9e6798be5
Sophievcy <sophievc@shaw.ca>
2025-03-19 01:26:17 -0700

    interaction models for latency and flight frequency

--- Baboon_stepwise_models.R
+++ Baboon_stepwise_models.R
@@ -19,3 +19,46 @@ Vigilance_interaction_model <- glmmTMB(proportion_vigilant_beta ~ Predator.cue *
 summary(Vigilance_interaction_model)
 AIC(Vigilance_best_model, Vigilance_interaction_model)
 #interactions are important
+
+#LATENCY TO FLEE
+latency_full_model <- glmmTMB(log_latency_to_flee ~ Predator.cue + Habitat + age_sex_class + Number.of.individuals + Presence_of_offspring + (1|Camera.trap.site),
+                              data = Baboon_flight_stats,
+                              ziformula = ~1,     # Zero-inflation formula
+                              family = gaussian(),
+                              na.action = na.fail)
+
+# Stepwise model selection based on AIC
+latency_best_model <- step(latency_full_model, direction = "backward")
+summary(latency_best_model)
+
+#Test for interaction effects
+latency_interaction_model <- glmmTMB(log_latency_to_flee ~ Predator.cue * Habitat + 
+                                         Number.of.individuals * Habitat + 
+                                         (1|Camera.trap.site),
+                                       data = Baboon_flight_stats, 
+                                       family = gaussian())
+
+summary(latency_interaction_model)
+AIC(latency_best_model, latency_interaction_model)
+#interactions are important - AIC for interaction model is lower
+
+#FREQUENCY OF FLIGHT
+frequency_full_model <- glmmTMB(flight_present ~ Predator.cue + Habitat + age_sex_class + Number.of.individuals + Presence_of_offspring + (1|Camera.trap.site),
+                                data = Baboon_frequency_stats,
+                                family = binomial(),
+                                na.action = na.fail)
+
+# Stepwise model selection based on AIC
+frequency_best_model <- step(frequency_full_model, direction = "backward")
+summary(frequency_best_model)
+
+#Test for interaction effects
+frequency_interaction_model <- glmmTMB(flight_present ~ Predator.cue * Habitat + 
+                                         Number.of.individuals * Habitat + 
+                                         (1|Camera.trap.site),
+                                       data = Baboon_frequency_stats, 
+                                       family = binomial())
+
+summary(frequency_interaction_model)
+AIC(frequency_best_model, frequency_interaction_model)
+#interactions are important - AIC for interaction model is lower
