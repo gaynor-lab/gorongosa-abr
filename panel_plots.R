f2467a57e7b15452b93276fcd24c02a493a7edf0
Sophievcy <sophievc@shaw.ca>
2025-03-25 16:17:51 -0700

    created panel plots to compare 2021 and 2024

--- panel_plots.R
+++ panel_plots.R
@@ -5,5 +5,35 @@ install.packages("cowplot")
 library(ggplot2)
 library(cowplot)
 
+#PREDATOR IDENTITY
+
 #Vigilance plots 
 plot_grid(vigilance_predcue_plot_21, vigilance_predcue_plot_24, labels = c("2021", "2024"), ncol = 2)
+
+#latency plots
+plot_grid(latency_predcue_plot_21, latency_predcue_plot_24, labels = c("2021", "2024"), ncol = 2)
+
+#frequency plots
+plot_grid(frequency_predcue_plot_21, frequency_predcue_plot_24, labels = c("2021", "2024"), ncol = 2)
+
+#HABITAT TYPE
+
+#vigilance plots
+plot_grid(vigilance_habitat_plot_21, vigilance_habitat_plot_24, labels = c("2021", "2024"), ncol = 2)
+
+#latency plots
+plot_grid(latency_habitat_plot_21, latency_habitat_plot_24, labels = c("2021", "2024"), ncol = 2)
+
+#frequency plots
+plot_grid(frequency_habitat_plot_21, frequency_habitat_plot_24, labels = c("2021", "2024"), ncol = 2)
+
+#PREY IDENTITY
+
+#vigilance plots
+plot_grid(vigilance_prey_plot_21, vigilance_prey_plot_24, labels = c("2021", "2024"), ncol = 2)
+
+#latency plots
+plot_grid(latency_prey_plot_21, latency_prey_plot_24, labels = c("2021", "2024"), ncol = 2)
+
+#frequency plots
+plot_grid(frequency_prey_plot_21, frequency_prey_plot_24, labels = c("2021", "2024"), ncol = 2)
